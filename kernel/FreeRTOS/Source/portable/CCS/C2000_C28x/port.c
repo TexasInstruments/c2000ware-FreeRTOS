137c1c52a11f45ab08b2384b5035a02b4d09ac1e
Siddharth Deshpande <a0132081@ti.com>
2026-05-20 22:22:13 +0530

    Updated to add C2000Ware v26.01.00.00.STS contents

--- kernel/FreeRTOS/Source/portable/CCS/C2000_C28x/port.c
+++ kernel/FreeRTOS/Source/portable/CCS/C2000_C28x/port.c
@@ -1,23 +1,30 @@
-//-------------------------------------------------------------------------------------------------
-// Author: Ivan Zaitsev, ivan.zaitsev@gmail.com
-//
-// This file follows the FreeRTOS distribution license.
-//
-// FreeRTOS is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License (version 2) as published by the
-// Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-//
-// ***************************************************************************
-// >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-// >>!   distribute a combined work that includes FreeRTOS without being   !<<
-// >>!   obliged to provide the source code for proprietary components     !<<
-// >>!   outside of the FreeRTOS kernel.                                   !<<
-// ***************************************************************************
-//
-// FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-// link: http://www.freertos.org/a00114.html
+/*
+ * FreeRTOS Kernel <DEVELOPMENT BRANCH>
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ */
 //-------------------------------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------------------------------
--- kernel/README.md
+++ kernel/README.md
@@ -24,6 +24,7 @@ List of devices supported
 - [F28P65x](https://www.ti.com/product/TMS320F28P650DK).
 - [F28P55x](https://www.ti.com/product/TMS320F28P559SJ-Q1).
 - [F28E12x](https://www.ti.com/product/F28E120SB).
+- [F28p551x](https://www.ti.com/product/TMS320F28P551SG).
 
 
 ## Repository Layout
